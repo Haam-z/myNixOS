05049e59c788d96f87c79a55d8a7119119edc3db
Hamza Zarrouk <hamzazarouk@gmail.com>
2022-12-01 14:35:35 +0100

    chanaged fish settings

--- home/packages/desktop.nix
+++ home/packages/desktop.nix
@@ -2,5 +2,5 @@
 
 
 {
-  home.packages = with pkgs; [ alacritty arandr cinnamon.nemo discord firefox flameshot libreoffice-fresh mailspring pavucontrol spotify tor-browser-bundle-bin virt-manager vlc zathura];
+  home.packages = with pkgs; [ani-cli alacritty arandr cinnamon.nemo discord firefox flameshot libreoffice-fresh mailspring pavucontrol spotify tor-browser-bundle-bin virt-manager vlc zathura];
 }
