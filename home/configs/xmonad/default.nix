7e6be7deab52b1845d5ba14bc0d647fdae410ec2
Hamza Zarrouk <hamzazarouk@gmail.com>
2022-12-20 15:58:46 +0100

    changed configs

--- home/configs/xmonad/default.nix
+++ home/configs/xmonad/default.nix
@@ -1,12 +1,8 @@
 { config, lib, pkgs, ... }:
 
 {
-  home.file.nixos_compil = {
-    source = ./nixos-compil.sh;
-    target = "/home/haam/.xmonad/scripts/nixos-compil.sh";
-  };
   home.file.xmoand = {
-    source = ./xmonad.hs;
-    target = "/home/haam/.xmonad/xmonad.hs";
+    source = ./xmonad;
+    target = "/home/haam/.xmonad";
   };
 }
