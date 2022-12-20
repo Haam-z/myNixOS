6b89b5555722bbc3cfc6361d9b5252d7ce98c560
Hamza Zarrouk <hamzazarouk@gmail.com>
2022-12-20 15:49:41 +0100

    changed configs

--- home/configs/xmonad/default.nix
+++ home/configs/xmonad/default.nix
@@ -2,11 +2,11 @@
 
 {
   home.file.scripts = {
-    source = ./xmonad/scripts ;
+    source = ./scripts ;
     target = "/home/haam/.xmonad/scripts";
   };
   home.file.xmoand = {
-    source = ./xmonad/xmonad.hs;
+    source = ./xmonad.hs;
     target = "/home/haam/.xmonad/xmonad.hs";
   };
 }
